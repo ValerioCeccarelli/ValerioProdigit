d9114c8946c634dccf20f03a9e3569b1448ed366
Valerio Ceccarelli <valerio.ceccarelli@outlook.it>
2022-10-02 00:17:07 +0200

    .

--- ValerioProdigit.Api/Dtos/Classroom/GetClassroomResponse.cs
+++ ValerioProdigit.Api/Dtos/Classroom/GetClassroomResponse.cs
@@ -5,9 +5,8 @@ public sealed class GetClassroomResponse
 	public bool Success => Error.Length == 0;
 	public string Error { get; set; } = "";
 
-	//todo: controllare
-	public string BuildingId { get; set; }
-	public string Code { get; set; }
-	public IEnumerable<int> Rows { get; set; }
+	public string BuildingId { get; set; } = default!;
+	public string Code { get; set; } = default!;
+	public IEnumerable<int> Rows { get; set; } = default!;
 	public int Capacity { get; set; }
 }

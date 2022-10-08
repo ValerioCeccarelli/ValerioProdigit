618a7306ffd31bbf42d7cf78b3d01095fa94f048
Valerio Ceccarelli <valerio.ceccarelli@outlook.it>
2022-10-08 18:55:13 +0200

    Enable or disable email service

--- ValerioProdigit.Api/Emails/ConfigureEmailServiceExtension.cs
+++ ValerioProdigit.Api/Emails/ConfigureEmailServiceExtension.cs
@@ -7,7 +7,14 @@ public static class ConfigureEmailServiceExtension
     public static void ConfigureEmailService(this WebApplicationBuilder builder)
     {
         var settings = builder.Configuration.GetSection("Settings:SendGridSettings").Get<SendGridSettings>();
-        var service = new SendGridEmailService(settings.Token, settings.ServiceEmail, settings.ServiceName);
-        builder.Services.AddSingleton<IEmailSender>(service);
+        if (settings.IsActive)
+        {
+            var service = new SendGridEmailService(settings.Token, settings.ServiceEmail, settings.ServiceName);
+            builder.Services.AddSingleton<IEmailSender>(service);
+        }
+        else
+        {
+            builder.Services.AddSingleton<IEmailSender>(new EmptyEmailService());
+        }
     }
 }
---
+++ ValerioProdigit.Api/Emails/EmptyEmailService.cs
@@ -0,0 +1,16 @@
+﻿using ValerioProdigit.Api.Models;
+
+namespace ValerioProdigit.Api.Emails;
+
+public class EmptyEmailService : IEmailSender
+{
+	public Task<bool> SendRegisterConfirmation(ApplicationUser user, string link)
+	{
+		return Task.FromResult(true);
+	}
+
+	public Task<bool> SendReservationCreated(ApplicationUser user, Reservation reservation)
+	{
+		return Task.FromResult(true);
+	}
+}
